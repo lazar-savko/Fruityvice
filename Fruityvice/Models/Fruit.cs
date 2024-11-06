73bfb31d5ae11b179ec7c9b30bd4de52ac87005c
Lazar Savkovic <166607081+lazar-savko@users.noreply.github.com>
2024-11-06 17:23:35 +0100

    Update Fruit.cs

--- Fruityvice/Models/Fruit.cs
+++ Fruityvice/Models/Fruit.cs
@@ -4,14 +4,21 @@ namespace Fruityvice.Models
 {
     public class Fruit
     {
+        [JsonPropertyName("id")]  // Adding this line to map the "id" from the JSON to the "Id" property
+        public int Id { get; set; }  // New Id property
+
         [JsonPropertyName("name")]
         public string Name { get; set; }
+
         [JsonPropertyName("genus")]
         public string Genus { get; set; }
+
         [JsonPropertyName("family")]
         public string Family { get; set; }
+
         [JsonPropertyName("order")]
         public string Order { get; set; }
+
         [JsonPropertyName("nutritions")]
         public Nutritions Nutritions { get; set; }
     }
